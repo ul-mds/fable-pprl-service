772b220ee65f7af02115bc3dbea8948efee257bb
Paul Brassel <52356233+pbrassel@users.noreply.github.com>
2026-06-09 15:05:26 +0200

    Similarity vectors for match endpoint (#5)
    * Similarity vectors for match endpoint
    
    * Bump package version

--- pprl_service/routers/match.py
+++ pprl_service/routers/match.py
@@ -1,10 +1,19 @@
 import binascii
+from itertools import product
 
 import fable_core
 from bitarray import bitarray
 from fastapi import APIRouter, HTTPException
+from fable_core.aggregation import AggregationFn
 from fable_core.similarity import SimilarityFn
-from fable_model import SimilarityMeasure, VectorMatchRequest, VectorMatchResponse, Match, MatchMethod
+from fable_model import (
+    SimilarityMeasure,
+    VectorMatchRequest,
+    VectorMatchResponse,
+    Match,
+    MatchMethod,
+    SimilarityAggregator,
+)
 from starlette import status
 
 router = APIRouter()
@@ -13,6 +22,16 @@ _similarity_mapping: dict[SimilarityMeasure, SimilarityFn] = {
     SimilarityMeasure.cosine: fable_core.similarity.cosine,
     SimilarityMeasure.dice: fable_core.similarity.dice,
     SimilarityMeasure.jaccard: fable_core.similarity.jaccard,
+    SimilarityMeasure.russell_rao: fable_core.similarity.russell_rao,
+    SimilarityMeasure.sokal_sneath: fable_core.similarity.sokal_sneath,
+    SimilarityMeasure.sokal_michener: fable_core.similarity.sokal_michener,
+    SimilarityMeasure.roger_tanimoto: fable_core.similarity.roger_tanimoto,
+}
+
+_aggregator_mapping: dict[SimilarityAggregator, AggregationFn] = {
+    SimilarityAggregator.avg: fable_core.aggregation.average,
+    SimilarityAggregator.max: fable_core.aggregation.maximum,
+    SimilarityAggregator.min: fable_core.aggregation.minimum,
 }
 
 
@@ -38,34 +57,29 @@ def _construct_bitarray_lookup_dict(match_req: VectorMatchRequest) -> dict[str, 
 
 @router.post("/")
 async def perform_matching(match_req: VectorMatchRequest) -> VectorMatchResponse:
-    sim_measure = match_req.config.measure
+    sim_measures = match_req.config.measures
     sim_method = match_req.config.method
-    sim_fn = _similarity_mapping.get(sim_measure)
+    sim_fns = [_similarity_mapping.get(sim_measure) for sim_measure in sim_measures]
+    agg_fn = _aggregator_mapping.get(match_req.config.aggregator)
 
-    if sim_fn is None:
+    for i, sim_fn in enumerate(sim_fns):
+        if sim_fn is None:
+            raise HTTPException(
+                status_code=status.HTTP_501_NOT_IMPLEMENTED,
+                detail=f"unimplemented similarity measure `{sim_measures[i].name}`",
+            )
+
+    if match_req.config.aggregator != SimilarityAggregator.none and agg_fn is None:
         raise HTTPException(
-            status_code=status.HTTP_501_NOT_IMPLEMENTED, detail=f"unimplemented similarity measure `{sim_measure.name}`"
+            status_code=status.HTTP_501_NOT_IMPLEMENTED,
+            detail=f"unimplemented aggregator `{match_req.config.aggregator.name}`",
         )
 
     bitarray_lookup = _construct_bitarray_lookup_dict(match_req)
     matches: list[Match] = []
 
     if sim_method == MatchMethod.crosswise:
-        for domain_entity in match_req.domain:
-            for range_entity in match_req.range:
-                domain_ba = bitarray_lookup[domain_entity.value]
-                range_ba = bitarray_lookup[range_entity.value]
-
-                similarity = sim_fn(domain_ba, range_ba)
-
-                if similarity >= match_req.config.threshold:
-                    matches.append(
-                        Match(
-                            domain=domain_entity,
-                            range=range_entity,
-                            similarity=similarity,
-                        )
-                    )
+        pairs = product(match_req.domain, match_req.range)
     elif sim_method == MatchMethod.pairwise:
         if len(match_req.domain) != len(match_req.range):
             raise HTTPException(
@@ -73,25 +87,42 @@ async def perform_matching(match_req: VectorMatchRequest) -> VectorMatchResponse
                 detail="length of domain and range lists do not match: domain has length of "
                 f"{len(match_req.domain)}, range has length of {len(match_req.range)}",
             )
-
-        for domain_entity, range_entity in zip(match_req.domain, match_req.range):
-            domain_ba = bitarray_lookup[domain_entity.value]
-            range_ba = bitarray_lookup[range_entity.value]
-
-            similarity = sim_fn(domain_ba, range_ba)
-
-            if similarity >= match_req.config.threshold:
-                matches.append(
-                    Match(
-                        domain=domain_entity,
-                        range=range_entity,
-                        similarity=similarity,
-                    )
-                )
+        pairs = zip(match_req.domain, match_req.range)
     else:
         raise HTTPException(
             status_code=status.HTTP_501_NOT_IMPLEMENTED, detail=f"unimplemented match method `{sim_method.name}`"
         )
+
+    for domain_entity, range_entity in pairs:
+        domain_ba = bitarray_lookup[domain_entity.value]
+        range_ba = bitarray_lookup[range_entity.value]
+
+        similarities = [sim_fn(domain_ba, range_ba) for sim_fn in sim_fns]
+
+        if match_req.config.aggregator == SimilarityAggregator.none:
+            agg_similarity = None
+            is_match = all(
+                [threshold <= similarity for threshold, similarity in zip(match_req.config.thresholds, similarities)]
+            )
+        else:
+            try:
+                agg_similarity = agg_fn(similarities, **match_req.config.aggregator_args)
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"error while aggregating similarities: {e}",
+                )
+            is_match = match_req.config.thresholds[0] <= agg_similarity
+
+        if is_match:
+            matches.append(
+                Match(
+                    domain=domain_entity,
+                    range=range_entity,
+                    similarities=similarities,
+                    aggregated_similarity=agg_similarity,
+                )
+            )
 
     return VectorMatchResponse(
         config=match_req.config,
--- tests/test_match.py
+++ tests/test_match.py
@@ -1,12 +1,20 @@
-from fable_model import MatchConfig, SimilarityMeasure, VectorMatchRequest, VectorMatchResponse, Match, MatchMethod
+from fable_model import (
+    MatchConfig,
+    SimilarityMeasure,
+    VectorMatchRequest,
+    VectorMatchResponse,
+    Match,
+    MatchMethod,
+    SimilarityAggregator,
+)
 from starlette import status
 
 
 def test_match_crosswise(test_client, bit_vector_entity_factory):
     exact_match_entity = bit_vector_entity_factory()
     config = MatchConfig(
-        measure=SimilarityMeasure.jaccard,
-        threshold=1,
+        measures=SimilarityMeasure.jaccard,
+        thresholds=1,
         method=MatchMethod.crosswise,
     )
     match_request = VectorMatchRequest(
@@ -21,14 +29,16 @@ def test_match_crosswise(test_client, bit_vector_entity_factory):
     match_response = VectorMatchResponse(**r.json())
 
     assert match_response.config == config
-    assert match_response.matches == [Match(domain=exact_match_entity, range=exact_match_entity, similarity=1)]
+    assert match_response.matches == [
+        Match(domain=exact_match_entity, range=exact_match_entity, similarities=[1], aggregated_similarity=None)
+    ]
 
 
 def test_match_pairwise(test_client, bit_vector_entity_factory):
     exact_match_entity = bit_vector_entity_factory()
     config = MatchConfig(
-        measure=SimilarityMeasure.jaccard,
-        threshold=1,
+        measures=SimilarityMeasure.jaccard,
+        thresholds=1,
         method=MatchMethod.pairwise,
     )
     match_request = VectorMatchRequest(
@@ -43,17 +53,68 @@ def test_match_pairwise(test_client, bit_vector_entity_factory):
     match_response = VectorMatchResponse(**r.json())
 
     assert match_response.config == config
-    assert match_response.matches == [Match(domain=exact_match_entity, range=exact_match_entity, similarity=1)]
+    assert match_response.matches == [
+        Match(domain=exact_match_entity, range=exact_match_entity, similarities=[1], aggregated_similarity=None)
+    ]
 
 
-def test_match_404_on_invalid_base64(test_client, bit_vector_entity_factory):
+def test_match_with_aggregator(test_client, bit_vector_entity_factory):
+    exact_match_entity = bit_vector_entity_factory()
+    config = MatchConfig(
+        measures=[SimilarityMeasure.jaccard, SimilarityMeasure.sokal_michener, SimilarityMeasure.roger_tanimoto],
+        thresholds=1,
+        method=MatchMethod.pairwise,
+        aggregator=SimilarityAggregator.avg,
+    )
+    match_request = VectorMatchRequest(
+        config=config,
+        domain=[exact_match_entity, bit_vector_entity_factory()],
+        range=[exact_match_entity, bit_vector_entity_factory()],
+    )
+
+    r = test_client.post("/match", json=match_request.model_dump())
+    assert r.status_code == status.HTTP_200_OK
+
+    match_response = VectorMatchResponse(**r.json())
+
+    assert match_response.config == config
+    assert match_response.matches == [
+        Match(domain=exact_match_entity, range=exact_match_entity, similarities=[1, 1, 1], aggregated_similarity=1)
+    ]
+
+
+def test_with_multiple_thresholds(test_client, bit_vector_entity_factory):
+    exact_match_entity = bit_vector_entity_factory()
+    config = MatchConfig(
+        measures=[SimilarityMeasure.jaccard, SimilarityMeasure.sokal_michener, SimilarityMeasure.roger_tanimoto],
+        thresholds=[1, 1, 1],
+        method=MatchMethod.pairwise,
+    )
+    match_request = VectorMatchRequest(
+        config=config,
+        domain=[exact_match_entity, bit_vector_entity_factory()],
+        range=[exact_match_entity, bit_vector_entity_factory()],
+    )
+
+    r = test_client.post("/match", json=match_request.model_dump())
+    assert r.status_code == status.HTTP_200_OK
+
+    match_response = VectorMatchResponse(**r.json())
+
+    assert match_response.config == config
+    assert match_response.matches == [
+        Match(domain=exact_match_entity, range=exact_match_entity, similarities=[1, 1, 1], aggregated_similarity=None)
+    ]
+
+
+def test_match_400_on_invalid_base64(test_client, bit_vector_entity_factory):
     valid_entity, invalid_entity = bit_vector_entity_factory(), bit_vector_entity_factory()
     invalid_entity.value = "=A="  # invalid character for b64
 
     match_request = VectorMatchRequest(
         config=MatchConfig(
-            measure=SimilarityMeasure.jaccard,
-            threshold=1,
+            measures=SimilarityMeasure.jaccard,
+            thresholds=1,
         ),
         domain=[valid_entity],
         range=[invalid_entity],
@@ -64,11 +125,11 @@ def test_match_404_on_invalid_base64(test_client, bit_vector_entity_factory):
     assert r.json()["detail"] == f"invalid Base64 encoded bit vectors on entities with IDs {invalid_entity.id}"
 
 
-def test_match_404_on_pairwise_unmatched_list_lengths(test_client, bit_vector_entity_factory):
+def test_match_400_on_pairwise_unmatched_list_lengths(test_client, bit_vector_entity_factory):
     match_request = VectorMatchRequest(
         config=MatchConfig(
-            measure=SimilarityMeasure.jaccard,
-            threshold=1,
+            measures=SimilarityMeasure.jaccard,
+            thresholds=1,
             method=MatchMethod.pairwise,
         ),
         domain=[bit_vector_entity_factory()] * 2,
@@ -79,4 +140,24 @@ def test_match_404_on_pairwise_unmatched_list_lengths(test_client, bit_vector_en
     assert r.status_code == status.HTTP_400_BAD_REQUEST
     assert r.json()["detail"] == (
         "length of domain and range lists do not match: domain has length of 2, range has length of 1"
+    )
+
+
+def test_match_500_on_aggregation(test_client, bit_vector_entity_factory):
+    match_request = VectorMatchRequest(
+        config=MatchConfig(
+            measures=[SimilarityMeasure.jaccard, SimilarityMeasure.sokal_michener, SimilarityMeasure.roger_tanimoto],
+            thresholds=1,
+            method=MatchMethod.pairwise,
+            aggregator=SimilarityAggregator.avg,
+            aggregator_args={"weights": [1, 2]},
+        ),
+        domain=[bit_vector_entity_factory()],
+        range=[bit_vector_entity_factory()],
+    )
+
+    r = test_client.post("/match", json=match_request.model_dump())
+    assert r.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert r.json()["detail"] == (
+        "error while aggregating similarities: There need to be as many weights as there are similarities."
     )

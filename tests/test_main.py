3a6933f71259f093f99f8140294b8ae1507514a3
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-07-28 10:47:33 +0200

    chore: use standardized dependency group spec

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,16 @@
+from fastapi.routing import APIRoute
 import pytest
 
 from pprl_service.config import Role
 from pprl_service.main import create_app
+
+
+def _get_all_routes(routes):
+    for route in routes:
+        if isinstance(route, APIRoute):
+            yield route
+        elif hasattr(route, "original_router"):
+            yield from _get_all_routes(route.original_router.routes)
 
 
 @pytest.mark.parametrize(
@@ -15,7 +24,7 @@ from pprl_service.main import create_app
 def test_different_roles(monkeypatch, role, expected_route_names, not_expected_route_names):
     monkeypatch.setenv("ROLE", role)
     app = create_app()
-    route_names = [route.name for route in app.routes]
+    route_names = [route.name for route in _get_all_routes(app.routes)]
 
     assert all([name in route_names for name in expected_route_names])
     assert all([name not in route_names for name in not_expected_route_names])
